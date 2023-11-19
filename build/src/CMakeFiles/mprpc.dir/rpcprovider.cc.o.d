587ba26ba5f2d3a7d03014d465f7c22b592d9673
unknown <1273911058@qq.com>
2023-11-19 20:20:33 +0800

    feat:frist push

---
+++ build/src/CMakeFiles/mprpc.dir/rpcprovider.cc.o.d
@@ -0,0 +1,300 @@
+src/CMakeFiles/mprpc.dir/rpcprovider.cc.o: \
+ /home/zjh/project/mprpc/src/rpcprovider.cc /usr/include/stdc-predef.h \
+ /home/zjh/project/mprpc/src/include/rpcprovider.h \
+ /usr/include/c++/4.8.2/memory /usr/include/c++/4.8.2/bits/stl_algobase.h \
+ /usr/include/c++/4.8.2/x86_64-redhat-linux/bits/c++config.h \
+ /usr/include/bits/wordsize.h \
+ /usr/include/c++/4.8.2/x86_64-redhat-linux/bits/os_defines.h \
+ /usr/include/features.h /usr/include/sys/cdefs.h \
+ /usr/include/gnu/stubs.h /usr/include/gnu/stubs-64.h \
+ /usr/include/c++/4.8.2/x86_64-redhat-linux/bits/cpu_defines.h \
+ /usr/include/c++/4.8.2/bits/functexcept.h \
+ /usr/include/c++/4.8.2/bits/exception_defines.h \
+ /usr/include/c++/4.8.2/bits/cpp_type_traits.h \
+ /usr/include/c++/4.8.2/ext/type_traits.h \
+ /usr/include/c++/4.8.2/ext/numeric_traits.h \
+ /usr/include/c++/4.8.2/bits/stl_pair.h \
+ /usr/include/c++/4.8.2/bits/move.h \
+ /usr/include/c++/4.8.2/bits/concept_check.h \
+ /usr/include/c++/4.8.2/type_traits \
+ /usr/include/c++/4.8.2/bits/stl_iterator_base_types.h \
+ /usr/include/c++/4.8.2/bits/stl_iterator_base_funcs.h \
+ /usr/include/c++/4.8.2/debug/debug.h \
+ /usr/include/c++/4.8.2/bits/stl_iterator.h \
+ /usr/include/c++/4.8.2/bits/allocator.h \
+ /usr/include/c++/4.8.2/x86_64-redhat-linux/bits/c++allocator.h \
+ /usr/include/c++/4.8.2/ext/new_allocator.h /usr/include/c++/4.8.2/new \
+ /usr/include/c++/4.8.2/exception \
+ /usr/include/c++/4.8.2/bits/atomic_lockfree_defines.h \
+ /usr/include/c++/4.8.2/bits/exception_ptr.h \
+ /usr/include/c++/4.8.2/bits/nested_exception.h \
+ /usr/include/c++/4.8.2/bits/memoryfwd.h \
+ /usr/include/c++/4.8.2/bits/stl_construct.h \
+ /usr/include/c++/4.8.2/ext/alloc_traits.h \
+ /usr/include/c++/4.8.2/bits/alloc_traits.h \
+ /usr/include/c++/4.8.2/bits/ptr_traits.h \
+ /usr/include/c++/4.8.2/bits/stl_uninitialized.h \
+ /usr/include/c++/4.8.2/bits/stl_tempbuf.h \
+ /usr/include/c++/4.8.2/bits/stl_raw_storage_iter.h \
+ /usr/include/c++/4.8.2/typeinfo /usr/include/c++/4.8.2/bits/hash_bytes.h \
+ /usr/include/c++/4.8.2/iosfwd /usr/include/c++/4.8.2/bits/stringfwd.h \
+ /usr/include/c++/4.8.2/bits/postypes.h /usr/include/c++/4.8.2/cwchar \
+ /usr/include/wchar.h /usr/include/stdio.h \
+ /usr/lib/gcc/x86_64-redhat-linux/4.8.5/include/stdarg.h \
+ /usr/include/bits/wchar.h \
+ /usr/lib/gcc/x86_64-redhat-linux/4.8.5/include/stddef.h \
+ /usr/include/xlocale.h /usr/include/c++/4.8.2/ext/atomicity.h \
+ /usr/include/c++/4.8.2/x86_64-redhat-linux/bits/gthr.h \
+ /usr/include/c++/4.8.2/x86_64-redhat-linux/bits/gthr-default.h \
+ /usr/include/pthread.h /usr/include/endian.h /usr/include/bits/endian.h \
+ /usr/include/bits/byteswap.h /usr/include/bits/types.h \
+ /usr/include/bits/typesizes.h /usr/include/bits/byteswap-16.h \
+ /usr/include/sched.h /usr/include/time.h /usr/include/bits/sched.h \
+ /usr/include/bits/time.h /usr/include/bits/timex.h \
+ /usr/include/bits/pthreadtypes.h /usr/include/bits/setjmp.h \
+ /usr/include/c++/4.8.2/x86_64-redhat-linux/bits/atomic_word.h \
+ /usr/include/c++/4.8.2/ext/concurrence.h \
+ /usr/include/c++/4.8.2/bits/stl_function.h \
+ /usr/include/c++/4.8.2/backward/binders.h \
+ /usr/include/c++/4.8.2/bits/uses_allocator.h \
+ /usr/include/c++/4.8.2/functional /usr/include/c++/4.8.2/tuple \
+ /usr/include/c++/4.8.2/utility /usr/include/c++/4.8.2/bits/stl_relops.h \
+ /usr/include/c++/4.8.2/initializer_list /usr/include/c++/4.8.2/array \
+ /usr/include/c++/4.8.2/stdexcept /usr/include/c++/4.8.2/string \
+ /usr/include/c++/4.8.2/bits/char_traits.h /usr/include/c++/4.8.2/cstdint \
+ /usr/lib/gcc/x86_64-redhat-linux/4.8.5/include/stdint.h \
+ /usr/include/stdint.h /usr/include/c++/4.8.2/bits/localefwd.h \
+ /usr/include/c++/4.8.2/x86_64-redhat-linux/bits/c++locale.h \
+ /usr/include/c++/4.8.2/clocale /usr/include/locale.h \
+ /usr/include/bits/locale.h /usr/include/c++/4.8.2/cctype \
+ /usr/include/ctype.h /usr/include/c++/4.8.2/bits/ostream_insert.h \
+ /usr/include/c++/4.8.2/bits/cxxabi_forced.h \
+ /usr/include/c++/4.8.2/bits/range_access.h \
+ /usr/include/c++/4.8.2/bits/basic_string.h \
+ /usr/include/c++/4.8.2/ext/string_conversions.h \
+ /usr/include/c++/4.8.2/cstdlib /usr/include/stdlib.h \
+ /usr/include/bits/waitflags.h /usr/include/bits/waitstatus.h \
+ /usr/include/sys/types.h /usr/include/sys/select.h \
+ /usr/include/bits/select.h /usr/include/bits/sigset.h \
+ /usr/include/sys/sysmacros.h /usr/include/alloca.h \
+ /usr/include/bits/stdlib-float.h /usr/include/c++/4.8.2/cstdio \
+ /usr/include/libio.h /usr/include/_G_config.h \
+ /usr/include/bits/stdio_lim.h /usr/include/bits/sys_errlist.h \
+ /usr/include/c++/4.8.2/cerrno /usr/include/errno.h \
+ /usr/include/bits/errno.h /usr/include/linux/errno.h \
+ /usr/include/asm/errno.h /usr/include/asm-generic/errno.h \
+ /usr/include/asm-generic/errno-base.h \
+ /usr/include/c++/4.8.2/bits/functional_hash.h \
+ /usr/include/c++/4.8.2/bits/basic_string.tcc \
+ /usr/include/c++/4.8.2/bits/unique_ptr.h \
+ /usr/include/c++/4.8.2/bits/shared_ptr.h \
+ /usr/include/c++/4.8.2/bits/shared_ptr_base.h \
+ /usr/include/c++/4.8.2/backward/auto_ptr.h \
+ /usr/include/muduo/net/TcpServer.h /usr/include/muduo/base/Atomic.h \
+ /usr/include/muduo/base/noncopyable.h /usr/include/muduo/base/Types.h \
+ /usr/include/string.h /usr/include/assert.h \
+ /usr/include/muduo/net/TcpConnection.h \
+ /usr/include/muduo/base/StringPiece.h /usr/include/muduo/net/Callbacks.h \
+ /usr/include/muduo/base/Timestamp.h /usr/include/muduo/base/copyable.h \
+ /usr/local/include/boost/operators.hpp /usr/include/c++/4.8.2/cstddef \
+ /usr/include/c++/4.8.2/iterator /usr/include/c++/4.8.2/ostream \
+ /usr/include/c++/4.8.2/ios /usr/include/c++/4.8.2/bits/ios_base.h \
+ /usr/include/c++/4.8.2/bits/locale_classes.h \
+ /usr/include/c++/4.8.2/bits/locale_classes.tcc \
+ /usr/include/c++/4.8.2/streambuf \
+ /usr/include/c++/4.8.2/bits/streambuf.tcc \
+ /usr/include/c++/4.8.2/bits/basic_ios.h \
+ /usr/include/c++/4.8.2/bits/locale_facets.h \
+ /usr/include/c++/4.8.2/cwctype /usr/include/wctype.h \
+ /usr/include/c++/4.8.2/x86_64-redhat-linux/bits/ctype_base.h \
+ /usr/include/c++/4.8.2/bits/streambuf_iterator.h \
+ /usr/include/c++/4.8.2/x86_64-redhat-linux/bits/ctype_inline.h \
+ /usr/include/c++/4.8.2/bits/locale_facets.tcc \
+ /usr/include/c++/4.8.2/bits/basic_ios.tcc \
+ /usr/include/c++/4.8.2/bits/ostream.tcc /usr/include/c++/4.8.2/istream \
+ /usr/include/c++/4.8.2/bits/istream.tcc \
+ /usr/include/c++/4.8.2/bits/stream_iterator.h \
+ /usr/local/include/boost/config.hpp \
+ /usr/local/include/boost/config/user.hpp \
+ /usr/local/include/boost/config/detail/select_compiler_config.hpp \
+ /usr/local/include/boost/config/compiler/gcc.hpp \
+ /usr/local/include/boost/config/detail/select_stdlib_config.hpp \
+ /usr/local/include/boost/config/stdlib/libstdcpp3.hpp \
+ /usr/include/unistd.h /usr/include/bits/posix_opt.h \
+ /usr/include/bits/environments.h /usr/include/bits/confname.h \
+ /usr/include/getopt.h \
+ /usr/local/include/boost/config/detail/select_platform_config.hpp \
+ /usr/local/include/boost/config/platform/linux.hpp \
+ /usr/local/include/boost/config/detail/posix_features.hpp \
+ /usr/local/include/boost/config/detail/suffix.hpp \
+ /usr/local/include/boost/config/helper_macros.hpp \
+ /usr/local/include/boost/config/detail/cxx_composite.hpp \
+ /usr/local/include/boost/detail/workaround.hpp \
+ /usr/local/include/boost/config/workaround.hpp \
+ /usr/local/include/boost/core/addressof.hpp \
+ /usr/include/muduo/net/Buffer.h /usr/include/muduo/net/Endian.h \
+ /usr/include/c++/4.8.2/algorithm /usr/include/c++/4.8.2/bits/stl_algo.h \
+ /usr/include/c++/4.8.2/bits/algorithmfwd.h \
+ /usr/include/c++/4.8.2/bits/stl_heap.h /usr/include/c++/4.8.2/random \
+ /usr/include/c++/4.8.2/cmath /usr/include/math.h \
+ /usr/include/bits/huge_val.h /usr/include/bits/huge_valf.h \
+ /usr/include/bits/huge_vall.h /usr/include/bits/inf.h \
+ /usr/include/bits/nan.h /usr/include/bits/mathdef.h \
+ /usr/include/bits/mathcalls.h /usr/include/c++/4.8.2/limits \
+ /usr/include/c++/4.8.2/bits/random.h /usr/include/c++/4.8.2/vector \
+ /usr/include/c++/4.8.2/bits/stl_vector.h \
+ /usr/include/c++/4.8.2/bits/stl_bvector.h \
+ /usr/include/c++/4.8.2/bits/vector.tcc \
+ /usr/include/c++/4.8.2/x86_64-redhat-linux/bits/opt_random.h \
+ /usr/lib/gcc/x86_64-redhat-linux/4.8.5/include/x86intrin.h \
+ /usr/lib/gcc/x86_64-redhat-linux/4.8.5/include/ia32intrin.h \
+ /usr/lib/gcc/x86_64-redhat-linux/4.8.5/include/mmintrin.h \
+ /usr/lib/gcc/x86_64-redhat-linux/4.8.5/include/xmmintrin.h \
+ /usr/lib/gcc/x86_64-redhat-linux/4.8.5/include/mm_malloc.h \
+ /usr/lib/gcc/x86_64-redhat-linux/4.8.5/include/emmintrin.h \
+ /usr/lib/gcc/x86_64-redhat-linux/4.8.5/include/immintrin.h \
+ /usr/lib/gcc/x86_64-redhat-linux/4.8.5/include/fxsrintrin.h \
+ /usr/lib/gcc/x86_64-redhat-linux/4.8.5/include/adxintrin.h \
+ /usr/include/c++/4.8.2/bits/random.tcc /usr/include/c++/4.8.2/numeric \
+ /usr/include/c++/4.8.2/bits/stl_numeric.h \
+ /usr/include/muduo/net/InetAddress.h /usr/include/netinet/in.h \
+ /usr/include/sys/socket.h /usr/include/sys/uio.h /usr/include/bits/uio.h \
+ /usr/include/bits/socket.h /usr/include/bits/socket_type.h \
+ /usr/include/bits/sockaddr.h /usr/include/asm/socket.h \
+ /usr/include/asm-generic/socket.h /usr/include/asm/sockios.h \
+ /usr/include/asm-generic/sockios.h /usr/include/bits/in.h \
+ /usr/local/include/boost/any.hpp \
+ /usr/local/include/boost/any/bad_any_cast.hpp \
+ /usr/local/include/boost/any/fwd.hpp \
+ /usr/local/include/boost/config/pragma_message.hpp \
+ /usr/local/include/boost/type_traits/alignment_of.hpp \
+ /usr/local/include/boost/type_traits/intrinsics.hpp \
+ /usr/local/include/boost/type_traits/detail/config.hpp \
+ /usr/local/include/boost/version.hpp \
+ /usr/local/include/boost/type_traits/integral_constant.hpp \
+ /usr/local/include/boost/any/detail/placeholder.hpp \
+ /usr/local/include/boost/type_index.hpp \
+ /usr/local/include/boost/type_index/stl_type_index.hpp \
+ /usr/local/include/boost/type_index/type_index_facade.hpp \
+ /usr/local/include/boost/container_hash/hash_fwd.hpp \
+ /usr/include/c++/4.8.2/cstring \
+ /usr/local/include/boost/static_assert.hpp \
+ /usr/local/include/boost/throw_exception.hpp \
+ /usr/local/include/boost/exception/exception.hpp \
+ /usr/local/include/boost/assert/source_location.hpp \
+ /usr/local/include/boost/current_function.hpp \
+ /usr/local/include/boost/cstdint.hpp \
+ /usr/local/include/boost/core/demangle.hpp \
+ /usr/include/c++/4.8.2/cxxabi.h \
+ /usr/include/c++/4.8.2/x86_64-redhat-linux/bits/cxxabi_tweaks.h \
+ /usr/local/include/boost/type_traits/conditional.hpp \
+ /usr/local/include/boost/type_traits/is_const.hpp \
+ /usr/local/include/boost/type_traits/is_reference.hpp \
+ /usr/local/include/boost/type_traits/is_lvalue_reference.hpp \
+ /usr/local/include/boost/type_traits/is_rvalue_reference.hpp \
+ /usr/local/include/boost/type_traits/is_volatile.hpp \
+ /usr/local/include/boost/type_traits/remove_cv.hpp \
+ /usr/local/include/boost/type_traits/remove_reference.hpp \
+ /usr/local/include/boost/type_traits/decay.hpp \
+ /usr/local/include/boost/type_traits/is_array.hpp \
+ /usr/local/include/boost/type_traits/is_function.hpp \
+ /usr/local/include/boost/type_traits/detail/is_function_cxx_11.hpp \
+ /usr/local/include/boost/type_traits/remove_bounds.hpp \
+ /usr/local/include/boost/type_traits/remove_extent.hpp \
+ /usr/local/include/boost/type_traits/add_pointer.hpp \
+ /usr/local/include/boost/type_traits/add_reference.hpp \
+ /usr/local/include/boost/core/enable_if.hpp \
+ /usr/local/include/boost/type_traits/is_same.hpp \
+ /usr/include/c++/4.8.2/map /usr/include/c++/4.8.2/bits/stl_tree.h \
+ /usr/include/c++/4.8.2/bits/stl_map.h \
+ /usr/include/c++/4.8.2/bits/stl_multimap.h \
+ /usr/include/muduo/net/EventLoop.h /usr/include/c++/4.8.2/atomic \
+ /usr/include/c++/4.8.2/bits/atomic_base.h \
+ /usr/lib/gcc/x86_64-redhat-linux/4.8.5/include/stdbool.h \
+ /usr/include/muduo/base/Mutex.h /usr/include/muduo/base/CurrentThread.h \
+ /usr/include/muduo/net/TimerId.h \
+ /usr/local/include/google/protobuf/descriptor.h \
+ /usr/include/c++/4.8.2/set /usr/include/c++/4.8.2/bits/stl_set.h \
+ /usr/include/c++/4.8.2/bits/stl_multiset.h \
+ /usr/local/include/google/protobuf/stubs/common.h \
+ /usr/include/c++/4.8.2/iostream \
+ /usr/local/include/google/protobuf/stubs/macros.h \
+ /usr/local/include/google/protobuf/stubs/platform_macros.h \
+ /usr/local/include/google/protobuf/stubs/port.h \
+ /usr/local/include/google/protobuf/port_def.inc /usr/include/byteswap.h \
+ /usr/local/include/google/protobuf/port_undef.inc \
+ /usr/local/include/google/protobuf/stubs/stringpiece.h \
+ /usr/local/include/google/protobuf/stubs/hash.h \
+ /usr/include/c++/4.8.2/unordered_map \
+ /usr/include/c++/4.8.2/bits/hashtable.h \
+ /usr/include/c++/4.8.2/bits/hashtable_policy.h \
+ /usr/include/c++/4.8.2/bits/unordered_map.h \
+ /usr/include/c++/4.8.2/unordered_set \
+ /usr/include/c++/4.8.2/bits/unordered_set.h \
+ /usr/local/include/google/protobuf/stubs/logging.h \
+ /usr/local/include/google/protobuf/stubs/status.h \
+ /usr/local/include/google/protobuf/stubs/mutex.h \
+ /usr/include/c++/4.8.2/mutex /usr/include/c++/4.8.2/chrono \
+ /usr/include/c++/4.8.2/ratio /usr/include/c++/4.8.2/ctime \
+ /usr/include/c++/4.8.2/system_error \
+ /usr/include/c++/4.8.2/x86_64-redhat-linux/bits/error_constants.h \
+ /usr/local/include/google/protobuf/stubs/once.h \
+ /usr/local/include/google/protobuf/port.h \
+ /home/zjh/project/mprpc/src/include/logger.h \
+ /home/zjh/project/mprpc/src/include/lockqueue.h \
+ /usr/include/c++/4.8.2/queue /usr/include/c++/4.8.2/deque \
+ /usr/include/c++/4.8.2/bits/stl_deque.h \
+ /usr/include/c++/4.8.2/bits/deque.tcc \
+ /usr/include/c++/4.8.2/bits/stl_queue.h /usr/include/c++/4.8.2/thread \
+ /usr/include/c++/4.8.2/condition_variable \
+ /usr/local/include/google/protobuf/service.h \
+ /usr/local/include/google/protobuf/stubs/callback.h \
+ /home/zjh/project/mprpc/src/include/mprpcapplication.h \
+ /home/zjh/project/mprpc/src/include/mprpcconfig.h \
+ /home/zjh/project/mprpc/src/include/mprpcchannel.h \
+ /usr/local/include/google/protobuf/message.h \
+ /usr/local/include/google/protobuf/stubs/casts.h \
+ /usr/local/include/google/protobuf/arena.h \
+ /usr/local/include/google/protobuf/arena_impl.h \
+ /usr/local/include/google/protobuf/generated_message_reflection.h \
+ /usr/local/include/google/protobuf/generated_enum_reflection.h \
+ /usr/local/include/google/protobuf/generated_enum_util.h \
+ /usr/local/include/google/protobuf/message_lite.h \
+ /usr/include/c++/4.8.2/climits \
+ /usr/lib/gcc/x86_64-redhat-linux/4.8.5/include/limits.h \
+ /usr/lib/gcc/x86_64-redhat-linux/4.8.5/include/syslimits.h \
+ /usr/include/limits.h /usr/include/bits/posix1_lim.h \
+ /usr/include/bits/local_lim.h /usr/include/linux/limits.h \
+ /usr/include/bits/posix2_lim.h /usr/include/bits/xopen_lim.h \
+ /usr/local/include/google/protobuf/io/coded_stream.h \
+ /usr/local/include/google/protobuf/stubs/strutil.h \
+ /usr/local/include/google/protobuf/metadata_lite.h \
+ /usr/local/include/google/protobuf/unknown_field_set.h \
+ /usr/local/include/google/protobuf/parse_context.h \
+ /usr/local/include/google/protobuf/io/zero_copy_stream.h \
+ /usr/local/include/google/protobuf/arenastring.h \
+ /usr/local/include/google/protobuf/implicit_weak_message.h \
+ /usr/local/include/google/protobuf/repeated_field.h \
+ /usr/local/include/google/protobuf/inlined_string_field.h \
+ /usr/local/include/google/protobuf/wire_format_lite.h \
+ /usr/local/include/google/protobuf/io/zero_copy_stream_impl_lite.h \
+ /usr/local/include/google/protobuf/stubs/stl_util.h \
+ /usr/local/include/google/protobuf/generated_message_util.h \
+ /usr/local/include/google/protobuf/any.h \
+ /usr/local/include/google/protobuf/has_bits.h \
+ /home/zjh/project/mprpc/src/include/mprpccontroller.h \
+ /home/zjh/project/mprpc/src/include/rpcheader.pb.h \
+ /usr/local/include/google/protobuf/generated_message_table_driven.h \
+ /usr/local/include/google/protobuf/map.h \
+ /usr/local/include/google/protobuf/map_type_handler.h \
+ /usr/local/include/google/protobuf/map_entry_lite.h \
+ /usr/local/include/google/protobuf/map_field_lite.h \
+ /usr/local/include/google/protobuf/extension_set.h \
+ /usr/include/c++/4.8.2/cassert \
+ /home/zjh/project/mprpc/src/include/zookeeperutil.h \
+ /usr/include/semaphore.h /usr/include/bits/semaphore.h \
+ /usr/local/include/zookeeper/zookeeper.h /usr/include/sys/time.h \
+ /usr/local/include/zookeeper/proto.h \
+ /usr/local/include/zookeeper/zookeeper_version.h \
+ /usr/local/include/zookeeper/recordio.h \
+ /usr/local/include/zookeeper/zookeeper.jute.h
---
+++ example/callee/friendservice.cc
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+#include "friend.pb.h"
+#include "mprpcapplication.h"
+#include "rpcprovider.h"
+#include "logger.h"
+
+class FriendService : public fixbug::FriendServiceRpc
+{
+public:
+    std::vector<std::string> GetFriendsList(uint32_t userid)
+    {
+        std::cout << "do GetFriendsList service!";
+        std::vector<std::string> vec;
+        vec.push_back("gao yang");
+        vec.push_back("liu hong");
+        vec.push_back("wang shuo");
+        return vec;
+    }
+
+    void GetFriendsList(::google::protobuf::RpcController* controller,
+                       const ::fixbug::GetFriendsListRequest* request,
+                       ::fixbug::GetFriendsListResponse* response,
+                       ::google::protobuf::Closure* done)
+    {
+        uint32_t userid = request->userid();
+
+        std::vector<std::string> friendsList = GetFriendsList(userid);
+
+        response->mutable_result()->set_errcode(0);
+        response->mutable_result()->set_errmsg("");
+        for (std::string &name : friendsList)
+        {
+            std::string *p = response->add_friends();
+            *p = name;
+        }
+        done->Run();
+        
+
+    }
+};
+
+int main(int argc, char **argv)
+{
+    LOG_INFO("frist log message!");
+    LOG_ERROR("%s:%s:%d", __FILE__, __FUNCTION__, __LINE__);
+
+    //调用框架的初始化操作
+    MprpcApplication::Init(argc, argv);
+
+    //一个网络服务对象，把FriendService对象发布到rpc节点上
+    RpcProvider provider;
+    provider.NotifyService(new FriendService()); 
+
+    //启动一个rpc服务发布节点，Run以后进入阻塞状态，等待远程rpc请求
+    provider.Run();
+    return 0;
+} 
